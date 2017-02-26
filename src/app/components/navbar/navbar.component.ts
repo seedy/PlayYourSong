0d26460b8b8f1590b85a43428e1c58c3b00b4952
seedy.dupuis <seedy.dupuis@gmail.com>
2017-02-26 02:01:09 +0100

    core : changed theme and set config to use and generate sass files

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'pys-navbar',
   templateUrl: 'navbar.component.html',
-  styleUrls: ['navbar.component.css']
+  styleUrls: ['navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
   isLoggedIn = false;
--- src/app/components/welcome-bar/welcome-bar.component.ts
+++ src/app/components/welcome-bar/welcome-bar.component.ts
@@ -3,17 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'pys-welcome-bar',
   templateUrl: './welcome-bar.component.html',
-  styleUrls: ['./welcome-bar.component.css']
+  styleUrls: ['welcome-bar.component.scss']
 })
 export class WelcomeBarComponent implements OnInit {
   tiles = [
-    {title:'Welcome to Play Your Song', cols: '4', rows: '1', color: ''},
-    {title:'Development is still in progress...', cols: '3', rows: '1', color: ''},
-    {title:'But it will soon come!', cols: '1', rows: '2', color: ''},
-    {title:'In hope to offer you new powers', cols: '3', rows: '1', color: ''},
-    {title:'Search your favorite songs cross-applications', cols: '4', rows: '1', color: ''},
-    {title:'Build up your playlist...', cols: '3', rows: '1', color: ''},
-    {title:'Within seconds!', cols: '1', rows: '1', color: ''},
+    {title:'Welcome to Play Your Song', cols: '4', rows: '6', color: '#ff7043'},
+    {title:'Development is still in progress...', cols: '3', rows: '3', color: '#ff8a65'},
+    {title:'It will soon come out!', cols: '1', rows: '6', color: '#80cbc4'},
+    {title:'In hope to offer you new powers', cols: '3', rows: '3', color: '#29b6f6'}
   ];
   constructor() { }
 

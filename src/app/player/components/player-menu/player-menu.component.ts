a7852c6498c267a222c8f347135cc6854a6c5941
seedy.dupuis <seedy.dupuis@gmail.com>
2017-03-02 23:59:17 +0100

    core : created module

---
+++ src/app/player/components/player-menu/player-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pys-player-menu',
+  templateUrl: 'player-menu.component.html',
+  styleUrls: ['player-menu.component.scss']
+})
+export class PlayerMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/player/components/track/track.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pys-track',
+  templateUrl: 'track.component.html',
+  styleUrls: ['track.component.scss']
+})
+export class TrackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

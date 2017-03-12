37ba1ffe66ce32e8b2de5a5aa56491ec1076c6c2
seedy.dupuis <seedy.dupuis@gmail.com>
2017-03-12 01:36:07 +0100

    feat : integration of new repeat button. handled eventing and changed design of play pause button

--- src/app/player/components/player-menu/player-menu.component.ts
+++ src/app/player/components/player-menu/player-menu.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PlaylistControlService } from '../../../shared/services/playlist-control/playlist-control.service';
+
 @Component({
   selector: 'pys-player-menu',
   templateUrl: './player-menu.component.html',
-  styleUrls: ['./player-menu.component.scss']
+  styleUrls: ['./player-menu.component.scss'],
+  providers: [PlaylistControlService]
 })
 export class PlayerMenuComponent implements OnInit {
   playing: boolean;
 
-  constructor() { }
+  constructor(private playlistControlService: PlaylistControlService) {
+
+  }
 
   ngOnInit() {
     this.playing = false;
@@ -16,6 +21,10 @@ export class PlayerMenuComponent implements OnInit {
 
   playPause():void {
     this.playing = !this.playing;
+  }
+
+  notifyRepeatMode(mode: string): void{
+    this.playlistControlService.repeatModeControlChange(mode);
   }
 
 

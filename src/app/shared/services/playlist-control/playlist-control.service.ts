1855bbf840d0eb84e9a47042599fba9409c33ce4
seedy.dupuis <seedy.dupuis@gmail.com>
2017-03-12 03:00:25 +0100

    feat : shuffle behaviour in playlist control service

--- src/app/shared/services/playlist-control/playlist-control.service.ts
+++ src/app/shared/services/playlist-control/playlist-control.service.ts
@@ -5,13 +5,19 @@ import { Subject } from 'rxjs/Subject';
 export class PlaylistControlService {
 
   private repeatModeSource = new Subject<string>();
+  private shuffleSource = new Subject<boolean>();
 
   constructor() { }
 
   repeatModeControl$ = this.repeatModeSource.asObservable();
+  shuffleControl$ = this.shuffleSource.asObservable();
 
   repeatModeControlChange(mode: string): void {
     this.repeatModeSource.next(mode);
+  }
+
+  shuffleControlChange(doShuffle: boolean): void {
+    this.shuffleSource.next(doShuffle);
   }
 
 }

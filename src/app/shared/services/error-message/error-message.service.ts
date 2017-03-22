972369682aee1d84850466331687c6ac1118bb91
seedy.dupuis <seedy.dupuis@gmail.com>
2017-03-23 00:11:41 +0100

    feat : finished key mapping in error message service

--- src/app/shared/services/error-message/error-message.service.ts
+++ src/app/shared/services/error-message/error-message.service.ts
@@ -5,34 +5,55 @@ export class ErrorMessageService {
 
   constructor() { }
 
-  mapErrorMessages(form, data): string[] {
+  mapErrorMessages(form, data): {} {
     return Object.keys(data)
-      .map((key) => {
-        if(!form.get(key)){
-          return null;
+      .reduce((keyHash, key) => {
+        const errorMessages = keyHash[key] = [];
+
+        if (!form.get(key)) {
+          return keyHash;
         }
+
+        if (form.get(key).pristine) {
+          return keyHash;
+        }
+
         const errors = form.get(key).errors;
+
+        if (errors === null) {
+          return keyHash;
+        }
+
         if (errors['required']) {
-          return this.getRequiredMessage(key, errors['required']);
+          errorMessages.push(this.getRequiredMessage(key, errors['required']));
         }
 
         if (errors['minlength']) {
-          return this.getLengthMessage(key, errors['minlength'].requiredLength, true);
+          errorMessages.push(this.getLengthMessage(key, errors['minlength'], true));
         }
 
         if (errors['maxlength']) {
-          return this.getLengthMessage(key, errors['maxlength'].requiredLength, false);
+          errorMessages.push(this.getLengthMessage(key, errors['maxlength'], false));
         }
 
-      });
+        if (errors['pattern']) {
+          errorMessages.push(this.getPatternMessage(key, errors['pattern']));
+        }
+
+        return keyHash;
+      }, {});
   }
 
-  private getRequiredMessage(key, value): string{
-    return value === true ? 'Field ' + key + ' is required' : '';
+  private getRequiredMessage(key: string, errorObject: boolean): string {
+    return errorObject === true ? 'Field ' + key + ' is required' : '';
   }
 
-  private getLengthMessage(key, value, isMinMax){
+  private getLengthMessage(key: string, errorObject: {actualLength: number, requiredLength: number}, isMinMax: boolean): string {
     const suffix = isMinMax ? 'at least ' : 'less than ';
-    return 'Field ' + key + ' must be ' + suffix + value + ' characters long';
+    return 'Field ' + key + ' must be ' + suffix + errorObject.requiredLength + ' characters long';
+  }
+
+  private getPatternMessage(key: string, errorObject: {actualValue: string, requiredPattern: string}): string {
+    return 'Field ' + key + ' does not respect the expected pattern';
   }
 }

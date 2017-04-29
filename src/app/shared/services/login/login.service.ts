25a1f1bf0c04f5a9ab87e978ee0f56b964da189a
seedy.dupuis <seedy.dupuis@gmail.com>
2017-04-30 01:42:09 +0200

    feat : loginService added login and registration

--- src/app/shared/services/login/login.service.ts
+++ src/app/shared/services/login/login.service.ts
@@ -1,14 +1,57 @@
-import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
+import {Injectable, Inject} from '@angular/core';
+import { Http } from '@angular/http';
+
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
+import {APP_CONFIG, AppConfig} from '../../../core/app-config';
 
 @Injectable()
 export class LoginService {
 
+  endpoint: string;
   redirectUrl: string;
 
-  constructor() { }
+  constructor(private http: Http, @Inject(APP_CONFIG) config: AppConfig) {
+    this.endpoint = config.apiEndpoint + '/account';
+  }
 
   isLoggedIn(): boolean {
-    return false;
+    return tokenNotExpired();
+  }
+
+  login(credentials): Observable<any> {
+    const url = this.endpoint + '/login';
+    const json = {
+      username: credentials.username,
+      password: credentials.password
+    };
+    return this.http.post(url, json)
+      .map((response) => response.json())
+      .map((response) => {
+        if (response.success) {
+          localStorage.setItem('token', response.token);
+        }
+
+        return response.success;
+      });
+  }
+
+  register(credentials): Observable<any> {
+    const url = this.endpoint + '/register';
+    const json = {
+      username: credentials.username,
+      email: credentials.email,
+      password: credentials.password
+    };
+    return this.http.post(url, JSON.stringify(json))
+      .map((response) => response.json())
+      .map((response) => {
+        if (response.success) {
+          localStorage.setItem('token', response.token);
+        }
+
+        return response.success;
+      });
   }
 }

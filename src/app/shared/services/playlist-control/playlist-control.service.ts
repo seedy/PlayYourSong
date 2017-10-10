be041cd696c34a5e6fb4d16fe3f6bd6fa354632c
seedy.dupuis <seedy.dupuis@gmail.com>
2017-10-10 10:37:27 +0200

    feat : playlist control added selection control and clearQueue control. + Code refactor

--- src/app/shared/services/playlist-control/playlist-control.service.ts
+++ src/app/shared/services/playlist-control/playlist-control.service.ts
@@ -8,12 +8,12 @@ import {CircularList} from '../../classes/circular-list';
 export class PlaylistControlService {
 
   // mock for dev
-  private mock = new CircularList([
+  private queue = new CircularList([
     new Track('Kingston town', '', 'youtube', 'w0c_dv0TUmU', 'info'),
-    new Track('Stairway to heaven', '', '', 'xYC_78PUrZg', 'queue_music'),
-    new Track('Riders on the storm', '', '', 'xYC_78PUrZg', 'touch_app')
+    new Track('Stairway to heaven', '', 'youtube', 'xYC_78PUrZg', 'queue_music'),
+    new Track('Riders on the storm', '', 'youtube', 'xYC_78PUrZg', 'touch_app')
   ]);
-  private playlist = new BehaviorSubject<CircularList<Track>>(this.mock);
+  private playlist = new BehaviorSubject<CircularList<Track>>(this.queue);
 
   private clearQueueSource = new Subject<void>();
   private repeatModeSource = new Subject<string>();
@@ -29,20 +29,47 @@ export class PlaylistControlService {
 
   constructor() { }
 
-  clearQueueControlChange(): void {
-    this.clearQueueSource.next();
+  public selectControl(track: Track): void {
+    this.queue.select(track);
+    this.propagateQueueChange();
   }
 
-  repeatModeControlChange(mode: string): void {
+  /******************************/
+  /*     Observable Actions     */
+  /******************************/
+
+  public clearQueueControlChange(): void {
+    this.clearQueueSource.next();
+    this.clearQueue();
+  }
+
+  // TODO : integrate to player
+  public repeatModeControlChange(mode: string): void {
     this.repeatModeSource.next(mode);
   }
 
-  shuffleControlChange(doShuffle: boolean): void {
+  public shuffleControlChange(doShuffle: boolean): void {
     this.shuffleSource.next(doShuffle);
   }
 
-  saveQueueControlChange(): void {
+  // TODO : implement data storage
+  public saveQueueControlChange(): void {
     this.saveQueueSource.next();
+  }
+
+  /******************************/
+  /*     Private Functions      */
+  /******************************/
+
+  private clearQueue(): void {
+    this.queue.clear();
+    this.propagateQueueChange();
+  }
+
+
+
+  private propagateQueueChange(): void {
+    this.playlist.next(this.queue);
   }
 
 }

5a2abf3bd4dfba3fe60415f8021ca4d3c6339054
seedy.dupuis <seedy.dupuis@gmail.com>
2017-10-10 10:34:45 +0200

    fix : remove specific instance of playlist control service from player menu. One instance needed in module

--- src/app/player/components/player-menu/player-menu.component.ts
+++ src/app/player/components/player-menu/player-menu.component.ts
@@ -5,8 +5,7 @@ import { PlaylistControlService } from '../../../shared/services/playlist-contro
 @Component({
   selector: 'pys-player-menu',
   templateUrl: './player-menu.component.html',
-  styleUrls: ['./player-menu.component.scss'],
-  providers: [PlaylistControlService]
+  styleUrls: ['./player-menu.component.scss']
 })
 export class PlayerMenuComponent implements OnInit {
   playing: boolean;

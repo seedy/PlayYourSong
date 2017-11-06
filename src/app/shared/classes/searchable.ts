903d7a51736d503b0a898be3b5703bf2a90f622e
seedy.dupuis <seedy.dupuis@gmail.com>
2017-11-06 22:40:31 +0100

    feat : searchable more precise type on attribute service

--- src/app/shared/classes/searchable.ts
+++ src/app/shared/classes/searchable.ts
@@ -1,9 +1,11 @@
+import {Injectable} from '@angular/core';
+
 export class Searchable {
 
   constructor(
     public name: string,
     public id: string,
-    public service: any,
+    public service: Injectable,
     public query: Function,
     public active: boolean
   ) {};

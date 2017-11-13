64ca1f9415a3a58e07bdda74571d727f94af6b25
seedy.dupuis <seedy.dupuis@gmail.com>
2017-11-13 22:56:39 +0100

    feat : classes stub added method new Searchable

--- src/testing/classes-stub.ts
+++ src/testing/classes-stub.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Track} from '../app/shared/classes/track';
+import {Searchable} from '../app/shared/classes/searchable';
 
 @Injectable()
 export class ClassesStub {
@@ -7,5 +8,9 @@ export class ClassesStub {
     return new Track(id, '', '', '', '');
   }
 
+  public newSearchable(id: string): Searchable {
+    const queryFn = jasmine.createSpy('query');
+    return new Searchable(id, '', '', queryFn, false);
+  }
 
 }
